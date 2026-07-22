8748d1e1004fdc1cb0822beb48fdd3a5ba9c3825
Haya junaid <ammarrana3649169@gmail.com>
2026-07-22 11:03:56 -0700

    add assignment 06 files

---
+++ assignment06/app.js
@@ -0,0 +1,8 @@
+let studentNames = ["mehmil","haya","zimal"];
+let studentNames2 = new Array();
+let subjects =["Math","English","Computer","Urdu"];
+
+console.log("Q1:", studentNames);
+console.log("Q2:", studentNames2);
+console.log("Q3:", subjects);
+
